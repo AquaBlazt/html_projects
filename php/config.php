d43fdda07212ddfe907ea8f9d090a929c72db32e
Erik Saul Rivera Reyes <40293937+AquaBlazt@users.noreply.github.com>
2023-03-31 03:49:27 -0700

    config changes

--- php/config.php
+++ php/config.php
@@ -1,5 +1,29 @@
+
 <?php
+$array = [];
 
-$conn = mysqli_connect('localhost','root','','admin_form');
+for ($i = 1; $i <= 10; $i++) {
 
-?>
+ if($i<4)
+ {
+  $array[$i] = "a";
+  echo $array[$i];
+ }
+ else if ($i >3 and $i < 8)
+ {
+  $array[$i] = "b";
+  echo $array[$i];
+ }
+ else if ($i>=8)
+ {
+  $array[$i] = "c";
+  echo $array[$i];
+ }
+ else
+ {
+  echo "error";
+  echo $array[$i];
+ }
+
+}
+
